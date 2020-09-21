dee660d12b71f28a980c945a0a7d7346cfb22f5e
Настя Настя <analiakh17@gmail.com>
2020-09-21 19:40:00 +0300

    Add MockCategory class - create car category

---
+++ ShopTask4/Data/mocks/MockCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace ShopTask4.Data.mocks
+{
+    using System.Collections.Generic;
+    using ShopTask4.Data.interfaces;
+    using ShopTask4.Data.Models;
+
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> AllCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category {CategoryName = "Electro", Description = "Ecologic type of cars"},
+                    new Category{ CategoryName = "Patrol", Description ="Classical type of cars"}
+                }
+            }
+            
+        }
+    }
+}

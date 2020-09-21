b380f4a2882ed66c7af8dc5c6420ccceb9b1befd
Настя Настя <analiakh17@gmail.com>
2020-09-21 18:26:04 +0300

    Add Car model

---
+++ ShopTask4/Data/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopTask4.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string Image { get; set; }
+        public ushort Price { get; set; }
+        public bool IsFavourite { get; set; }
+        public int Awaitable { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}

bd367708965606e9b1d38ed025458c091a396ba7
Настя Настя <analiakh17@gmail.com>
2020-09-21 18:28:01 +0300

    add property to Car category

--- ShopTask4/Data/Models/Category.cs
+++ ShopTask4/Data/Models/Category.cs
@@ -1,9 +1,11 @@
 ﻿namespace ShopTask4.Data.Models
 {
+    using System.Collections.Generic;
     public class Category
     {
         public int  Id { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }
+        public List<Car> Cars { get; set; }
     }
 }

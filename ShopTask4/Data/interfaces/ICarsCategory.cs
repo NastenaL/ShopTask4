83b5b53a8df256226c8aa0c013edae88fa8ead8d
Настя Настя <analiakh17@gmail.com>
2020-09-21 18:30:46 +0300

    Add ICarsCategory interface

---
+++ ShopTask4/Data/interfaces/ICarsCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopTask4.Data.interfaces
+{
+    using ShopTask4.Data.Models;
+    using System.Collections.Generic;
+
+    interface ICarsCategory
+    {
+       IEnumerable<Category> AllCategories { get; }
+    }
+}

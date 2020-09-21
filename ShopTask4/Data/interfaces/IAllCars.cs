93a4e596de03d3a04c77d51d4c6a4a7f930aafee
Настя Настя <analiakh17@gmail.com>
2020-09-21 19:22:28 +0300

    Add IAllCars interface

---
+++ ShopTask4/Data/interfaces/IAllCars.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopTask4.Data.interfaces
+{
+    using ShopTask4.Data.Models;
+    using System.Collections.Generic;
+
+    interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; set; }
+        IEnumerable<Car> GetFavoriteCars { get; set; }
+        Car GetCar { get; set; }
+    }
+}

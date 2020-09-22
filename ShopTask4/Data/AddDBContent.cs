e6dd2cd8827a81808e9318cf876cf058f6c9032a
Настя Настя <analiakh17@gmail.com>
2020-09-22 13:04:40 +0300

    add file settings for data base

---
+++ ShopTask4/Data/AddDBContent.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopTask4.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using ShopTask4.Data.Models;
+
+    public class AddDBContent : DbContext
+    {
+        public AddDBContent(DbContextOptions<AddDBContent> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- ShopTask4/Startup.cs
+++ ShopTask4/Startup.cs
@@ -2,16 +2,27 @@
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using ShopTask4.Data;
     using ShopTask4.Data.interfaces;
     using ShopTask4.Data.mocks;
 
     public class Startup
     {
+        private IConfigurationRoot confString;
+        
+        public Startup(IHostingEnvironment hostEnv)
+        {
+            confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AddDBContent>(options => options.UseSqlServer(confString.GetConnectionString("DefaultConnection")));
             services.AddMvc();
             services.AddTransient<IAllCars, MockCars>();
             services.AddTransient<ICarsCategory, MockCategory>();
